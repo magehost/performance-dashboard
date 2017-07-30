15d06f537b47f240fa81a1535bb072672b9fbfd9
Jeroen Vermeulen <jeroen@magehost.pro>
2017-07-30 23:55:36 +0200

    Logging non cachable templates via Logger class

---
+++ Logger/Handler.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace MageHost\PerformanceDashboard\Logger;
+
+use Monolog\Logger;
+
+class Handler extends \Monolog\Handler\RotatingFileHandler
+{
+    /** @var \Magento\Framework\Filesystem\DirectoryList */
+    private $directoryList;
+
+    /**
+     * Logging level
+     * @param \Magento\Framework\Filesystem\DirectoryList $directoryList
+     * @param int $filename
+     * @param int $maxFiles
+     * @param bool|int $level
+     * @param bool $bubble
+     * @param null $filePermission
+     * @param bool $useLocking
+     * @internal param int $
+     */
+    public function __construct(
+        \Magento\Framework\Filesystem\DirectoryList $directoryList,
+        $filename,
+        $maxFiles = 0,
+        $level = Logger::DEBUG,
+        $bubble = true,
+        $filePermission = null,
+        $useLocking = false
+    ) {
+        $this->directoryList = $directoryList;
+        parent::__construct($filename, $maxFiles, $level, $bubble, $filePermission, $useLocking);
+    }
+
+    protected function getTimedFilename()
+    {
+        if ('/' != substr($this->filename, 0, 1)) {
+            // Prepend Magento log dir
+            $this->filename = sprintf("%s/%s", $this->directoryList->getPath('log'), $this->filename);
+        }
+        return parent::getTimedFilename();
+    }
+}
---
+++ Logger/Logger.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace MageHost\PerformanceDashboard\Logger;
+
+class Logger extends \Monolog\Logger
+{
+    const I_AM_NOT_EMPTY = true;
+}
